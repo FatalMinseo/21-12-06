46b99a2f799e477c85416753f8366a5c2767480b
FatalMinseo <95062577+FatalMinseo@users.noreply.github.com>
2021-12-06 16:17:30 +0900

    Update MusicMain.java

--- MusicMain.java
+++ MusicMain.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.ArrayList; //Î©îÏù∏
 import java.util.Scanner;
 
 import javazoom.jl.player.MP3Player;
@@ -10,31 +10,31 @@ public class MusicMain {
 		Scanner sc = new Scanner(System.in);
 		MP3Player mp3 = new MP3Player();
 
-//		Music music1 =new Music("±¯", "Rain", 100);
+//		Music music1 =new Music("Íπ°", "Rain", 100);
 //		Music music2 =new Music("Dalla Dalla", "Itzy", 120);
 //		Music music3 =new Music("Solo", "JEMMIE", 200);
 
 //		ArrayList<Music> musicList = new ArrayList<Music>();
-//		musicList.add(new Music("π˙Ω· 12Ω√", "√ª«œ", 100, "C://music/CHUNG HA - π˙Ω· 12Ω√.mp3"));
+//		musicList.add(new Music("Î≤åÏç® 12Ïãú", "Ï≤≠Ìïò", 100, "C://music/CHUNG HA - Î≤åÏç® 12Ïãú.mp3"));
 //		musicList.add(new Music("Dalla Dalla", "Itzy", 120, "C://music/Itzy - Dalla Dalla.mp3"));
 //		musicList.add(new Music("Solo", "JEMMIE", 200, "C://music/JENNIE - SOLO.mp3"));
 //		musicList.add(new Music("2002", "Anne Marie", 200, "C://music/Anne Marie - 2002.mp3"));
 //		musicList.add(new Music("Let It Go", "Idina Menzel", 200, "C://music/Idina Menzel - Let It Go.mp3"));
 //		musicList.add(new Music("A Whole New World", "Massoud, Naomi Scott", 200, "C://music/Mena Massoud, Naomi Scott - A Whole New World.mp3"));
-//		musicList.add(new Music("Rain", "±¯", 200, "C://music/Rain - ±¯.mp3"));
+//		musicList.add(new Music("Rain", "Íπ°", 200, "C://music/Rain - Íπ°.mp3"));
 //		musicList.add(new Music("Ring Ding Dong", "SHINee", 200, "C://music/SHINee - Ring Ding Dong.mp3"));
 //		musicList.add(new Music("FANCY", "TWICE", 200, "C://music/TWICE - FANCY.mp3"));
-//		musicList.add(new Music("NO ONE", "¿Ã«œ¿Ã", 200, "C://music/¿Ã«œ¿Ã - NO ONE.mp3"));
+//		musicList.add(new Music("NO ONE", "Ïù¥ÌïòÏù¥", 200, "C://music/Ïù¥ÌïòÏù¥ - NO ONE.mp3"));
 
 //		int index = 0;
 		MusicPlayer Player = new MusicPlayer();
 		
 		while (true) {
-			System.out.print("[1]¢∫ [2]°· [3]¢∫¢∫ [4]¢∏¢∏ [5]¢¡ >> ");
+			System.out.print("[1]‚ñ∂ [2]‚ñ† [3]‚ñ∂‚ñ∂ [4]‚óÄ‚óÄ [5]‚äô >> ");
 			int menu = sc.nextInt();
 
 			if (menu == 1) {
-				System.out.print("¢∫ >> ");
+				System.out.print("‚ñ∂ >> ");
 //				mp3.play(musicList.get(index).getPath());
 				show(Player.musicList, Player.index);
 				Player.play();
@@ -42,7 +42,7 @@ public class MusicMain {
 				if (mp3.isPlaying() == true) {
 					mp3.stop();
 				}
-				System.out.println("°·");
+				System.out.println("‚ñ†");
 			} else if (menu == 3) {
 				Player.nextPlay();
 				show(Player.musicList, Player.index);
@@ -50,10 +50,10 @@ public class MusicMain {
 				Player.prevPlay();
 				show(Player.musicList, Player.index);
 			} else if (menu == 5) {
-				System.out.println("«¡∑Œ±◊∑• ¡æ∑·");
+				System.out.println("ÌîÑÎ°úÍ∑∏Îû® Ï¢ÖÎ£å");
 				break;
 			} else {
-				System.out.println("¥ŸΩ√ ¿‘∑¬");
+				System.out.println("Îã§Ïãú ÏûÖÎ†•");
 			}
 			System.out.println();
 		}
@@ -62,9 +62,9 @@ public class MusicMain {
 
 	public static void show(ArrayList<Music> musicList, int index) {
 		System.out.println(musicList.get(index).getMusicName() + ", " + musicList.get(index).getSinger() + ", "
-				+ (musicList.get(index).getPlayTime() / 60) + "∫– " + musicList.get(index).getPlayTime() % 60 + "√ ");
+				+ (musicList.get(index).getPlayTime() / 60) + "Î∂Ñ " + musicList.get(index).getPlayTime() % 60 + "Ï¥à");
 	}
 }
 
-// C://music/Rain - ±¯.mp3
+// C://music/Rain - Íπ°.mp3
 // C://music/Itzy - Dalla Dalla.mp3 C://music/JENNIE - SOLO.mp3
